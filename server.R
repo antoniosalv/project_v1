f0e0edfc0084363f4a3a253c789b614cb04bb18d
Antonio Salvador <antoniosalv2018@gmail.com>
2018-05-01 18:58:42 -0400

    first commit

---
+++ server.R
@@ -0,0 +1,163 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(dplyr)
+library(googleVis)
+library(maps)
+library(ggplot2)
+library(plotly)
+library(shinydashboard)
+library(shinythemes)
+library(rsconnect)
+library(hexbin)
+
+data= read.csv("./NPAO_Cons_v4.csv", stringsAsFactors = FALSE)
+summaryvariables= read.csv("summaryvariables4.csv", stringsAsFactors = FALSE)
+data$Year = as.factor(data$Year)
+data$State = as.factor(data$State)
+
+shinyServer(function(input, output){
+  output$graph1 <- renderPlot(
+    data %>% 
+    filter(., Topic == 'PercentAdultsObesity') %>% 
+    filter(State == input$State) %>%
+    group_by(Year) %>% 
+    ggplot(aes(x = Year, y = Value)) +
+    geom_boxplot(fill="darkblue", alpha=0.5) + 
+    ggtitle("Adults That Have Obesity")+
+    theme(plot.title = element_text(hjust = 0.6))+
+    ylab("%") +
+    theme(axis.text=element_text(size=18),
+    axis.title=element_text(size=18,face="bold"))+
+    theme_economist() + scale_fill_economist()
+    #theme_stata() +scale_fill_economist()+
+    #theme_minimal() +scale_fill_solarized()+
+    #theme_gdocs() + scale_fill_gdocs()#+
+    #theme_tufte() #+ scale_fill_tableau()
+  )
+  output$graph2 <- renderPlot(
+    data %>% 
+      filter(., Topic == 'PercentAdultsNoPhysicalActivity') %>% 
+      filter(State == input$State) %>%
+      group_by(Year) %>% 
+      ggplot(aes(x = Year, y = Value)) +
+      geom_boxplot(fill="blue", alpha=0.8) + 
+      ggtitle("No Physical Activity") +
+      theme(plot.title = element_text(hjust = 0.3))+
+      ylab("%") +
+      theme(axis.text=element_text(size=18),
+            axis.title=element_text(size=18,face="bold"))+
+    theme_economist() + scale_fill_economist()
+    #theme_stata() #+scale_fill_economist()+
+    #theme_minimal() +scale_fill_solarized()+
+    #theme_gdocs() + scale_fill_gdocs()#+
+    #theme_tufte() #+ scale_fill_tableau()
+    
+  )
+  output$graph3 <- renderPlot(
+    data %>% 
+      filter(., Topic == 'PercentConsumingVegLessOneTimeDaily') %>% 
+      filter(State == input$State) %>%
+      group_by(Year) %>% 
+      ggplot(aes(x = Year, y = Value)) +
+      geom_boxplot(fill="darkblue", alpha=0.8) + 
+      ggtitle("Veg. Less Than One Day")+
+      theme(plot.title = element_text(hjust = 0.5))+
+      ylab("%")+
+      theme(axis.text=element_text(size=18),
+            axis.title=element_text(size=18,face="bold"))+
+    theme_economist() + scale_fill_economist()
+    #theme_stata() +scale_fill_economist() #+
+    #theme_minimal() +scale_fill_solarized()#+
+    #theme_gdocs() + scale_fill_gdocs()+
+    #theme_tufte() #+ scale_fill_tableau()
+  )
+  
+  output$graph4 <- renderPlotly(
+    data %>% 
+      filter(., Topic == input$Topic) %>% 
+      filter(., Topic != "StateAdCompleteStreetsPolicy") %>%
+      filter(State != 'National') %>% 
+      filter(., Year =='2012' & Year != '2016') %>% 
+      plot_geo(., locationmode = 'USA-states') %>%
+      add_trace(
+        z = ~Value, text = ~ValueOrig, locations = ~StateAbbr,
+        color = ~Value, colors='Purples', reversescale=TRUE, showscale=FALSE
+      ) %>%
+      #colorbar(title = "StatePolicy", xpad ='10') %>%
+      layout(
+        title = 'State Level Food Council',
+        geo = list(scope = 'usa',
+          projection = list(type = 'usa'),
+          showlakes = TRUE,
+          lakecolor = toRGB('white'))
+              )
+                      )
+  
+  output$graph5 <- renderPlotly(
+    data %>% 
+      filter(., Topic == input$Topic) %>% 
+      filter(State != 'National') %>% 
+      filter(., Year =='2011') %>% 
+      plot_geo(., locationmode = 'USA-states') %>%
+      add_trace(
+        z = ~Value, text = ~ValueOrig, locations = ~StateAbbr,
+        color = ~Value, colors='Greens', reversescale=TRUE, showscale=FALSE
+      ) %>%
+      #colorbar(title = "StatePolicy", xpad ='10') %>%
+      layout(
+        title = 'Policy for Healthier Food Retail',
+        geo = list(scope = 'usa',
+                   projection = list(type = 'albers usa'),
+                   showlakes = TRUE,
+                   lakecolor = toRGB('white'))
+              )
+                    )
+  
+
+  output$graph6 <- renderPlotly(
+    data %>% 
+      filter(., Topic == input$Topic) %>% 
+      filter(., Topic != "ExistStateLevelFoodPolicyCouncil") %>%
+      filter(State != 'National') %>% 
+      filter(., Year =='2016' & Year !='2012') %>% 
+      plot_geo(., locationmode = 'USA-states') %>%
+      add_trace(
+        z = ~Value, text = ~ValueOrig, locations = ~StateAbbr,
+        color = ~Value, colors='Blues', reversescale=TRUE, showscale=FALSE
+      ) %>%
+      colorbar(title = "StatePolicy", xpad ='10') %>%
+      layout(
+        title = 'Complete Street Policy -2016',
+        geo = list(scope = 'usa',
+                   projection = list(type = 'albers usa'),
+                   showlakes = TRUE,
+                   lakecolor = toRGB('white'))
+            ) 
+                )
+  
+  output$graph7 <- renderGvis(gvisBubbleChart(summaryvariables, 
+                    idvar="StateAbbr", 
+                    xvar = "AvgOW", 
+                    yvar="Value1",
+                    colorvar = "TOPIC",
+                    sizevar ="AvgOW",
+                    options=list(width=1000, height=600,
+                    title ="Risk Factor Obesity vs Environment Factors",
+                    hAxis="{title: '% Obesity'}",
+                    vAxis ="{title: '%'}",
+                    legend= "{position: 'in'}", #bottom top left right none
+                    bubble ="{textStyle: {fontSize:  10}}"
+                             )
+                   )
+                )
+  
+  }
+)
---
+++ ui.R
@@ -0,0 +1,97 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+library(shiny)
+library(dplyr)
+library(googleVis)
+library(maps)
+library(ggplot2)
+library(plotly)
+library(shinydashboard)
+library(shinythemes)
+library(rsconnect)
+library(hexbin)
+
+data= read.csv("./NPAO_Cons_v4.csv", stringsAsFactors = FALSE)
+summaryvariables= read.csv("summaryvariables4.csv", stringsAsFactors = FALSE)
+data$Year = as.factor(data$Year)
+data$State = as.factor(data$State)
+
+
+
+shinyUI(dashboardPage(
+  dashboardHeader(title="US Health: Exploring Risk Factors, Policies and Environment",
+                  titleWidth = 650), 
+  
+  dashboardSidebar(
+    sidebarMenu(
+      fluidRow(
+        column(12,
+               selectizeInput(inputId = "State",
+                              label = "State", 
+                              choices = unique(data$State)
+                              )  
+              )
+              ),
+      
+      fluidRow(
+        column(12,
+               selectizeInput(inputId = "Topic",
+                              label = "Topic",
+                              choices = c("ExistStateLevelFoodPolicyCouncil", 
+                              "PolicyHealthierFoodRetail", 
+                              "StateAdCompleteStreetsPolicy")
+                              )
+              )
+            )
+      )
+    ),
+  
+#main panel section
+  dashboardBody(fluidRow(
+             column(width=4, height = 100,
+              mainPanel(plotOutput("graph1", width = "150%", height = "300px"))
+                    ),
+              column(width=4, height =100,
+                mainPanel(plotOutput("graph2", width = '150%', height = '300px'))
+                     ),
+              column(width=4, height = 100,
+                mainPanel(plotOutput("graph3", width = '150%', height = '300px'))
+                     )  
+                    ),
+             br(),
+             br(),
+             br(),
+             
+            fluidRow(
+             column(4, height=300,
+                  mainPanel(plotlyOutput("graph4", width = "200%", height = "300px"))
+                    ),
+            column(4, height=300,
+                   mainPanel(plotlyOutput("graph5", width = '200%', height = '300px'))
+                    ),
+            column(width = 4, height=300,
+                    mainPanel(plotlyOutput("graph6", width = '200%', height = '300px')) 
+                   )
+                   ),
+            br(),
+            br(),
+            br(),
+            
+           fluidRow(
+             column(width = 12, height=100,
+                    mainPanel(htmlOutput('graph7', width = '150%', height = '200px'))
+                    )
+                   )
+           
+           )
+    )
+  )
+
+
+
